79bfd85453b18f75ed974ca40451e5dcf6a78b4f
Kir Chou <note351@hotmail.com>
2021-02-17 18:03:29 +0900

    clean LR0 parser code

--- LR0/parser.py
+++ LR0/parser.py
@@ -1,16 +1,16 @@
 # https://leetcode.com/problems/basic-calculator-ii/discuss/1068095/Python-Lexer-%2B-LR(0)-Parser-with-DFA-and-parsing-table
 
-# For non-operator => (number of symb , reduced state, None)
-# For operator => (number of number, operator, reduced state)
+# (number of popped symb, derived symb)
 reduce_rules = {
-    1: (0, 'Done', None),
-    2: (3, '+', 'E'),
-    3: (3, '-', 'E'),
-    4: (1, 'E', None),
-    5: (3, '*', 'T'),
-    6: (3, '/', 'T'),
-    7: (1, 'T', None)
+    1: (0, 'Done'),  # E'-> E
+    2: (3, 'E'),     # E -> E+T
+    3: (3, 'E'),     # E -> E-T
+    4: (1, 'E'),     # E -> T
+    5: (3, 'T'),     # T -> T*N
+    6: (3, 'T'),     # T -> T/N
+    7: (1, 'T')      # T -> N
 }
+
 # negative => reduce / positive => shift
 parsing_table = {
     1: {'N': 12, 'E': 2, 'T': 3},
@@ -27,57 +27,54 @@ parsing_table = {
     12:{'+': -7, '-': -7, '$': -7, '*': -7, '/': -7, 'N': -7},
 }
 
-
 class ParserLR0:
     def __init__(self, E: str) -> None:
         self.E = E
         self.pos = 0
-        self.states = [1]
+        self.states = [1]  # set initial state to 1
         self.symbs = []  # Example value => [('N', 1), ('+', '+')]
         
     def symb2key(self, symb: str) -> str:
         # raw symb can only be either '+-*/' or 'N'
         return symb if symb in ['$', '+', '-', '*', '/'] else 'N'
 
-    def parse(self) -> int:
+    def parse(self):
         while True:
             symb = self.E[self.pos]
             nxt_state = parsing_table[self.states[-1]][self.symb2key(symb)]
             if nxt_state > 0:
                 # shift
                 self.states.append(nxt_state)
-                self.symbs.append((self.symb2key(symb), symb))
+                self.symbs.append(symb)
                 self.pos += 1
             else:
                 # reduce
-                pop_n, action, nxt_key = reduce_rules[-nxt_state]
-                if action == 'Done':
+                pop_n, derived_symb = reduce_rules[-nxt_state]
+                if pop_n == 0:
                     # assume all input is valid, symbs will leave the answer
-                    return self.symbs[0][1]
+                    return self.symbs[0]
                 else:
-                    if pop_n == 1:  # simple reduction based on parsing table
+                    if pop_n == 1:  # simple reduction
                         # reduce states
-                        prev_state = self.states.pop()
-                        self.states.append(parsing_table[self.states[-1]][action])
-                        # derivation 
-                        _, prev_symb = self.symbs.pop()
-                        self.symbs.append((action, prev_symb))
-                    elif pop_n == 3:  # operator reduction based on parsing table
+                        self.states.pop()
+                        self.states.append(parsing_table[self.states[-1]][derived_symb])
+                        # derivation
+                        # Since reduce from T -> N and E -> T don't change the symbol value
+                        # namely, self.symbs.append(self.symbs.pop()) == pass
+                    elif pop_n == 3:  # operator reduction
                         # reduce states
-                        prev_state = self.states.pop()
-                        prev_state = self.states.pop()
-                        # get lval and rval + calculate the result + derivation
-                        rkey, rval = self.symbs.pop()
-                        _, _ = self.symbs.pop()  # don't care operator, we actually don't need store it as symbol
-                        lkey, lval = self.symbs.pop()
-                        if action == '+':
-                            self.symbs.append((nxt_key, lval + rval))
-                        elif action == '-':
-                            self.symbs.append((nxt_key, lval - rval))
-                        elif action == '*':
-                            self.symbs.append((nxt_key, lval * rval))
-                        elif action == '/':
-                            self.symbs.append((nxt_key, lval // rval))
+                        self.states = self.states[:-2]  # pop 2 states
+                        # derivation: get lval, operator, and rval => calculate the result
+                        lval, operator, rval = self.symbs[-3:]
+                        self.symbs = self.symbs[:-3]  # pop 3 symbols
+                        if operator == '+':
+                            self.symbs.append(lval + rval)
+                        elif operator == '-':
+                            self.symbs.append(lval - rval)
+                        elif operator == '*':
+                            self.symbs.append(lval * rval)
+                        elif operator == '/':
+                            self.symbs.append(lval // rval)
 
 
 class Solution:    
